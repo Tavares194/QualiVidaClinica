dfab747b271814c99743424fea2b0744249f8a62
Gabriel Tavares <gabriel.tavares.1904@gmail.com>
2023-11-14 17:35:39 -0300

    Fix Nav-Bar and Create Sign-up Validations

--- qualivida-clinica/public/scripts/cadastro.js
+++ qualivida-clinica/public/scripts/cadastro.js
@@ -5,6 +5,7 @@ const cpfInput = document.querySelector('#cpf');
 const rgInput = document.querySelector('#rg');
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#senha');
+const confirmPasswordInput = document.querySelector('#confirmarSenha');
 
 document.addEventListener('DOMContentLoaded', () => {
   if (mensagem_erro) {
@@ -32,42 +33,136 @@ passwordInput.addEventListener('focus', event => {
   passwordInput.classList.remove('is-invalid')
 })
 
-cadastroForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+confirmPasswordInput.addEventListener('focus', event => {
+  passwordInput.classList.remove('is-invalid')
+})
+
+function isNumeric(input) {
+  return /^\d+$/.test(input);
+}
+
+function hasNumber(input) {
+  return /\d/.test(input);
+}
+
+function hasUpperCase(input) {
+  return /[A-Z]/.test(input);
+}
+
+function hasLowerCase(input) {
+  return /[a-z]/.test(input);
+}
+
+function validateCPF(strCPF) {
+  var soma;
+  var resto;
+  soma = 0;
+  if (strCPF == "00000000000") return false;
+
+  for (i = 1; i <= 9; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+  resto = (soma * 10) % 11;
+
+  if ((resto == 10) || (resto == 11)) resto = 0;
+  if (resto != parseInt(strCPF.substring(9, 10))) return false;
+
+  soma = 0;
+  for (i = 1; i <= 10; i++) soma = soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+  resto = (soma * 10) % 11;
+
+  if ((resto == 10) || (resto == 11)) resto = 0;
+  if (resto != parseInt(strCPF.substring(10, 11))) return false;
+  return true;
+}
+
+cpfInput.addEventListener('input', function () {
+  var v = this.value;
+
+  if (isNaN(v[v.length - 1])) {
+    this.value = v.substring(0, v.length - 1);
+    return;
+  }
+
+  this.setAttribute("maxlength", "14");
+  if (v.length == 3 || v.length == 7) this.value += ".";
+  if (v.length == 11) this.value += "-";
+});
+
+function validateRG(strRG) {
+  const numerosRG = strRG.replace(/\D/g, '').slice(0, -1);
+
+  if (numerosRG.length !== 9) {
+    return false;
+  }
+
+  let soma = 0;
+  for (let i = 0; i < 8; i++) {
+    soma += parseInt(numerosRG.charAt(i)) * (8 - i);
+  }
+
+  const resto = soma % 11;
+  let digitoVerificador = 11 - resto;
+  if (digitoVerificador === 10 || digitoVerificador === 11) {
+    digitoVerificador = 0;
+  }
+
+  const ultimoDigito = parseInt(strRG.charAt(strRG.length - 1));
+  return digitoVerificador === ultimoDigito;
+}
+
+rgInput.addEventListener('input', function () {
+  var v = this.value;
+
+  if (isNaN(v[v.length - 1])) {
+    this.value = v.substring(0, v.length - 1);
+    return;
+  }
+
+  this.setAttribute("maxlength", "12");
+  if (v.length == 2 || v.length == 6) this.value += ".";
+  if (v.length == 10) this.value += "-";
+});
+
+
+cadastroForm.addEventListener('submit', event => {
+  const inputs = document.querySelectorAll('input');
   let formError;
 
-  if (!nomeInput.value) {
-    formError = 'Preencha o campo nome!';
-    nomeInput.classList.add('is-invalid');
+  let hasEmptyField = false;
+  inputs.forEach(input => {
+    if (!input.value) {
+      hasEmptyField = true;
+      inputs.classList.add('is-invalid');
+      return;
+    }
+  })
+
+  if (hasEmptyField) {
+    formError += 'Todos os campos devem ser preenchidos!';
+    return;
   }
 
-  if (!cpfInput.value) {
-    formError = 'Preencha o campo CPF!';
-    cpfInput.classList.add('is-invalid');
+  if (!hasNumber(passwordInput.value) || !hasUpperCase(passwordInput.value) || !hasLowerCase(passwordInput.value)) {
+    formError += 'A senha criada não atende aos requisitos!';
+    return;
   }
 
-  if (!rgInput.value) {
-    formError = 'Preencha o campo RG!';
-    rgInput.classList.add('is-invalid');
+  if (confirmPasswordInput.value != password) {
+    formError += 'As senhas não coincidem!';
+    return;
   }
 
-  if (!emailInput.value) {
-    formError = 'Preencha o campo email!';
-    emailInput.classList.add('is-invalid');
+  cpfInput.value = cpfInput.value.replace(/[.-]/g, '');
+
+  if (!validateCPF(cpfInput.value)) {
+    formError += 'CPF Inválido!';
+    return;
   }
 
-  if (!passwordInput.value) {
-    formError = 'Preencha o campo senha!';
-    passwordInput.classList.add('is-invalid');
-  }
+  rgInput.value = rgInput.value.replace(/[.-]/g, '');
 
-  if (!nomeInput.value && !cpfInput.value && !rgInput.value && !emailInput.value && !passwordInput.value) {
-    formError = 'Preencha todos os campos!';
-    nomeInput.classList.add('is-invalid');
-    cpfInput.classList.add('is-invalid');
-    rgInput.classList.add('is-invalid');
-    emailInput.classList.add('is-invalid');
-    passwordInput.classList.add('is-invalid');
+  if (!validateRG(rgInput.value)) {
+    formError += 'RG Inválido!';
+    return;
   }
 
   if (formError) {
@@ -77,3 +172,50 @@ cadastroForm.addEventListener('submit', (event) => {
     cadastroForm.submit();
   }
 })
+
+// if (!nomeInput.value) {
+//   formError = 'Preencha o campo nome!';
+//   nomeInput.classList.add('is-invalid');
+// }
+
+// if (!cpfInput.value) {
+//   formError = 'Preencha o campo CPF!';
+//   cpfInput.classList.add('is-invalid');
+// }
+
+// if (!rgInput.value) {
+//   formError = 'Preencha o campo RG!';
+//   rgInput.classList.add('is-invalid');
+// }
+
+// if (!emailInput.value) {
+//   formError = 'Preencha o campo email!';
+//   emailInput.classList.add('is-invalid');
+// }
+
+// if (!passwordInput.value) {
+//   formError = 'Preencha o campo senha!';
+//   passwordInput.classList.add('is-invalid');
+// }
+
+// if (!confirmPasswordInput.value) {
+//   formError = 'Preencha o campo confirmar senha!';
+//   confirmPasswordInput.classList.add('is-invalid');
+// }
+
+// if (!nomeInput.value && !cpfInput.value && !rgInput.value && !emailInput.value && !passwordInput.value && !confirmPasswordInput.value) {
+//   formError = 'Preencha todos os campos!';
+//   nomeInput.classList.add('is-invalid');
+//   cpfInput.classList.add('is-invalid');
+//   rgInput.classList.add('is-invalid');
+//   emailInput.classList.add('is-invalid');
+//   passwordInput.classList.add('is-invalid');
+//   confirmPasswordInput.classList.add('is-invalid');
+// }
+
+// if (formError) {
+//   errorDiv.innerText = formError;
+// } else {
+//   errorDiv.innerText = '';
+//   cadastroForm.submit();
+// }
